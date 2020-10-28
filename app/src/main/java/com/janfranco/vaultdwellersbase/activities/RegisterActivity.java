a3882bc4393e2beb43edda5f8980221212f4a10f
Eren Sırakaya <poisonweeb@gmail.com>
2020-10-29 01:03:59 +0300

    initial

---
+++ app/src/main/java/com/janfranco/vaultdwellersbase/activities/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.janfranco.vaultdwellersbase.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.janfranco.vaultdwellersbase.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+    
+}
